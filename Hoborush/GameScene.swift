92e529062f4180f60718f20b23b52e4ac978cc75
Ferdinando <113529408+frigoGen@users.noreply.github.com>
2022-12-07 19:33:34 +0100

    cose magiche richiede spiegazione

--- Hoborush/GameScene.swift
+++ Hoborush/GameScene.swift
@@ -9,9 +9,14 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    var touchLeft : Bool = false
+    var touchRight : Bool = false
+    var player : SKSpriteNode!
+    //var player : SKSpriteNode!
     //let label = SKLabelNode(text: "Hello SpriteKit!")
-    let player = SKSpriteNode(imageNamed: "hobo2")
+    //let player = SKSpriteNode(imageNamed: "HoboIdle1")
+    var attAnimation:SKAction!
+    var idleanimation:SKAction!
     var background = SKSpriteNode(imageNamed: "back")
     /*@objc func tap(recognizer: UIGestureRecognizer) {
      let viewLocation = recognizer.location(in: view)
@@ -25,20 +30,70 @@ class GameScene: SKScene {
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         // 4
         
-        addChild(player)
+        //addChild(player)
+        idleAnimation()
         run(SKAction.repeatForever(
               SKAction.sequence([
                 SKAction.run(addMonster),
                 SKAction.wait(forDuration: 1.0)
                 ])
             ))
-        addChild(background)
+        HoboAttack()
         
+       // addChild(background)
+        //SKAction.removeFromParent()
+        //let actionMove = SKAction.run(HoboAttack)
 
         
     }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    func HoboAttack()
+    {
+        let att1 = SKTexture(imageNamed: "HoboAttack1")
+        /*player = SKSpriteNode(texture: att1)
+        player.size = CGSize(width: 288, height: 192)
+        player.position = CGPoint(x: size.width/2 + 64, y: player.size.height/2)*/
+        let att2 = SKTexture(imageNamed: "HoboAttack2")
+        let att3 = SKTexture(imageNamed: "HoboAttack3")
+        let att4 = SKTexture(imageNamed: "HoboAttack4")
+        let att5 = SKTexture(imageNamed: "HoboAttack5")
+        let att6 = SKTexture(imageNamed: "HoboAttack6")
+        let att7 = SKTexture(imageNamed: "HoboAttack7")
+        let att8 = SKTexture(imageNamed: "HoboAttack8")
+        
+        attAnimation = SKAction.animate(with: [att1, att2, att3, att4, att5,att6,att7,att8], timePerFrame: 0.05)
+        if(touchRight){
+            player.removeAction(forKey: "animate")
+            //addChild(player)
+            /*let actionMove = SKAction.repeat(attAnimation, count: 1)
+            let actionMoveDone = SKAction.removeFromParent()*/
+            //player.run(SKAction.sequence([actionMove, actionMoveDone]))
+            player.run(attAnimation,completion:{
+                self.player.run(self.idleanimation,withKey: "animate")
+           })
+            touchRight = false
+        }
+        else{
+            print("TODO")
+        }
+    }
+    func idleAnimation()
+    {
+        let idle1 = SKTexture(imageNamed: "HoboIdle1")
+        //idle = SKSpriteNode(texture: idle1)
+        player.size = CGSize(width: 128, height: 128)
+        player.position = CGPoint(x: size.width/2, y: player.size.height/2)
+        addChild(player)
+        
+        let idle2 = SKTexture(imageNamed: "HoboIdle2")
+        let idle3 = SKTexture(imageNamed: "HoboIdle3")
+        let idle4 = SKTexture(imageNamed: "HoboIdle4")
+        let idle5 = SKTexture(imageNamed: "HoboIdle5")
+        idleanimation = SKAction.repeatForever(SKAction.animate(with: [idle1, idle2, idle3, idle4, idle5], timePerFrame: 0.20))
+        
+        player.run(idleanimation, withKey: "animate")
     }
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
@@ -47,12 +102,32 @@ class GameScene: SKScene {
     func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max - min) + min
     }
-    
+    override func touchesBegan(_ touches: Set<UITouch>,with event: UIEvent?){
+        //var touchMiddle : Bool = false
+
+        for touch in (touches) {
+            let location = touch.location(in: self)
+
+            if(location.x < size.width/2){
+                touchLeft = true
+                touchRight = false
+                print("Left")
+            } else {
+                touchRight = true
+                touchLeft = false
+                HoboAttack()
+                print("Right")
+            } /*else {  //x is between -width / 4 and width / 4
+                touchMiddle = true
+                print("Middle")
+            }*/
+        }
+    }
     func addMonster() {
         let whack = random(min: 0.0, max: 1.0)
         var num:CGFloat = 1
         // Create sprite
-        let monster = SKSpriteNode(imageNamed: "Stick")
+        let monster = SKSpriteNode(imageNamed: "alieno")
         monster.size = CGSize(width: 128.0, height: 128.0)
         // Determine where to spawn the monster along the Y axis
         let actualY = monster.size.height/2 //random(min: monster.size.height/2, max: size.height - monster.size.height/2)
