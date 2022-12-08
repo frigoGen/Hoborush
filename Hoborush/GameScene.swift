23f97f9450102a3eb63e037b8b1279705f906278
Ferdinando <113529408+frigoGen@users.noreply.github.com>
2022-12-09 00:47:34 +0100

    cazzoni camminanti

--- Hoborush/GameScene.swift
+++ Hoborush/GameScene.swift
@@ -8,6 +8,11 @@
 import SpriteKit
 import GameplayKit
 
+public var deathAnimation : SKAction!
+public var attAnimation: SKAction!
+public var idleanimation: SKAction!
+public var alienWalkingAnimation: SKAction!
+
 struct PhysicsCategory {
   static let none      : UInt32 = 0
   static let all       : UInt32 = UInt32.max
@@ -19,85 +24,63 @@ class GameScene: SKScene {
     var touchLeft : Bool = false
     var touchRight : Bool = false
     var turn : Bool = false
-    //var player : SKSpriteNode!
-    var attAnimation: SKAction!
-    var idleanimation: SKAction!
-    //var player : SKSpriteNode!
+    
+    var monstersDestroyed = 0
     //let label = SKLabelNode(text: "Hello SpriteKit!")
     var player = SKSpriteNode(imageNamed: "HoboIdle1")
     var background = SKSpriteNode(imageNamed: "back")
-    /*@objc func tap(recognizer: UIGestureRecognizer) {
-     let viewLocation = recognizer.location(in: view)
-     let sceneLocation = convertPoint(fromView: viewLocation)
-     let moveToAction = SKAction.move(to: sceneLocation, duration: 1)
-     label.run(moveToAction)
-     }*/
     override func didMove(to view: SKView) {
         background.position = CGPoint(x: size.width, y: size.height)
-       // addChild(background)
+        // addChild(background)
         idleAnimation()
+        AlienWalkAn()
+        deathAn()
+        HoboAttack()
+        Idle()
         run(SKAction.repeatForever(
-              SKAction.sequence([
+            SKAction.sequence([
                 SKAction.run(addMonster),
                 SKAction.wait(forDuration: 1.0)
-                ])
-            ))
+            ])
+        ))
         //HoboAttack()
         //SKAction.removeFromParent()
         //let actionMove = SKAction.run(HoboAttack)
-
+        
         
     }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
-    func HoboAttack()
-    {
-        var attArr: [SKTexture] = []
-        for i in 1...8 {
-            //var scaler = "HoboAttack\(i)"
-            //scaler.resize(
-            attArr.append(SKTexture(imageNamed: "HoboAttack\(i)"))
-        }
-        attAnimation = SKAction.animate(with: attArr, timePerFrame: 0.03)
+    func Attack(){
         if(touchRight){
-            turn = true
+            turn = false
             if(!(player.xScale > 0)){
                 player.xScale = player.xScale * -1
             }
-            player.removeAction(forKey: "animate")
-            player.size = CGSize(width: 254, height: 182)
-            self.player.position = CGPoint(x: self.size.width/2, y: self.size.height/2 + 30)
-            player.run(attAnimation,completion:{
-                self.player.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
-                self.player.size = CGSize(width: 128, height: 128)
-                self.player.run(self.idleanimation,withKey: "animate")
-           })
-            turn = false
+            //turn = false
         }
         else if(touchLeft){
             turn = true
             if((player.xScale > 0)){
                 player.xScale = player.xScale * -1
             }
-            player.removeAction(forKey: "animate")
-            player.size = CGSize(width: 254, height: 182)
-            self.player.position = CGPoint(x: self.size.width/2, y: self.size.height/2 + 30)
-            player.run(attAnimation,completion:{
-                self.player.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
-                self.player.size = CGSize(width: 128, height: 128)
-                self.player.run(self.idleanimation,withKey: "animate")
-           })
-            turn = false
         }
+    player.removeAction(forKey: "animate")
+    player.size = CGSize(width: 254, height: 182)
+    player.position = CGPoint(x: self.size.width/2, y: self.size.height/2 + 30)
+    player.run(attAnimation,completion:{
+        self.player.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        self.player.size = CGSize(width: 128, height: 128)
+        self.player.run(idleanimation,withKey: "animate")
+    })
     }
-    func idleAnimation()
-    {
-        var idleArr: [SKTexture] = []
-        for i in 1...5 {
-            idleArr.append(SKTexture(imageNamed: "HoboIdle\(i)"))
-        }
-        idleanimation = SKAction.repeatForever(SKAction.animate(with: idleArr, timePerFrame: 0.15))
+    func Die(){
+    player.removeAction(forKey: "animate")
+    player.run(deathAnimation,completion:{
+    })
+    }
+    func Idle(){
         player.size = CGSize(width: 128, height: 128)
         player.position = CGPoint(x: size.width/2, y: size.height/2)
         player.run(idleanimation, withKey: "animate")
@@ -109,30 +92,27 @@ class GameScene: SKScene {
         player.physicsBody?.categoryBitMask = PhysicsCategory.player
         player.physicsBody?.contactTestBitMask = PhysicsCategory.monster
         player.physicsBody?.collisionBitMask = PhysicsCategory.none
-        player.physicsBody?.usesPreciseCollisionDetection = true
+        player.physicsBody?.usesPreciseCollisionDetection = false
     }
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     }
-    
     func random(min: CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max - min) + min
     }
     override func touchesBegan(_ touches: Set<UITouch>,with event: UIEvent?){
-        //var touchMiddle : Bool = false
-
         for touch in (touches) {
             let location = touch.location(in: self)
 
             if(location.x < size.width/2){
                 touchLeft = true
                 touchRight = false
-                HoboAttack()
+                Attack()
                 print("Left")
             } else if(location.x > size.width/2) {
                 touchRight = true
                 touchLeft = false
-                HoboAttack()
+                Attack()
                 print("Right")
             } /*else {  //x is between -width / 4 and width / 4
                 touchMiddle = true
@@ -144,7 +124,7 @@ class GameScene: SKScene {
         let whack = random(min: 0.0, max: 1.0)
         var num:CGFloat = 1
         // Create sprite
-        let monster = SKSpriteNode(imageNamed: "alieno")
+        let monster = SKSpriteNode(imageNamed: "AlienWalking1")
         monster.size = CGSize(width: 128.0, height: 128.0)
         // Determine where to spawn the monster along the Y axis
         let actualY = size.height/2 //random(min: monster.size.height/2, max: size.height - monster.size.height/2)
@@ -157,7 +137,10 @@ class GameScene: SKScene {
         // Position the monster slightly off-screen along the right edge,
         // and along a random position along the Y axis as calculated above
         monster.position = CGPoint(x: num*(size.width), y: actualY)
-        
+        monster.run(alienWalkingAnimation,withKey: "alienWalk")
+        if(!(monster.position.x > 0)){
+            monster.xScale = monster.xScale * -1
+        }
         // Add the monster to the scene
         addChild(monster)
         monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size) // 1
@@ -172,12 +155,26 @@ class GameScene: SKScene {
         let actionMove = SKAction.move(to: CGPoint(x: size.width/2, y: actualY),
                                        duration: TimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
-        monster.run(SKAction.sequence([actionMove, actionMoveDone]))
+        let loseAction = SKAction.run() { [weak self] in
+          guard let `self` = self else { return }
+          let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+            let gameOverScene = GameOverScene(size: self.size, won: false)
+          self.view?.presentScene(gameOverScene, transition: reveal)
+        }
+        monster.run(SKAction.sequence([actionMove, loseAction, actionMoveDone]))
+
     }
     func baseballBatDidCollideWithMonster(player: SKSpriteNode, monster: SKSpriteNode) {
       print("Hit")
-      //projectile.removeFromParent()
+        
       monster.removeFromParent()
+        monstersDestroyed += 1
+        if monstersDestroyed > 1 {
+          let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+          let gameOverScene = GameOverScene(size: self.size, won: true)
+          view?.presentScene(gameOverScene, transition: reveal)
+        }
+
     }
 }
 extension GameScene: SKPhysicsContactDelegate {
@@ -197,7 +194,7 @@ extension GameScene: SKPhysicsContactDelegate {
       if ((firstBody.categoryBitMask & PhysicsCategory.monster != 0) &&
           (secondBody.categoryBitMask & PhysicsCategory.player != 0)) {
         if let monster = firstBody.node as? SKSpriteNode,
-          let projectile = secondBody.node as? SKSpriteNode {
+          let player = secondBody.node as? SKSpriteNode {
             if((touchRight == true && monster.position.x > size.width/2) || (touchLeft == true && monster.position.x<size.width/2)){
                 baseballBatDidCollideWithMonster(player: player, monster: monster)
                 if(touchLeft){touchLeft = false}
@@ -208,3 +205,40 @@ extension GameScene: SKPhysicsContactDelegate {
     }
 
 }
+public class GameOverScene: SKScene {
+  init(size: CGSize, won:Bool) {
+    super.init(size: size)
+    
+    // 1
+    backgroundColor = SKColor.white
+    
+    // 2
+    let message = won ? "You Won!" : "You Lose :["
+    
+    // 3
+    let label = SKLabelNode(fontNamed: "Chalkduster")
+    label.text = message
+    label.fontSize = 40
+    label.fontColor = SKColor.black
+    label.position = CGPoint(x: size.width/2, y: size.height/2)
+    addChild(label)
+    
+    // 4
+    run(SKAction.sequence([
+      SKAction.wait(forDuration: 3.0),
+      SKAction.run() { [weak self] in
+        // 5
+        guard let `self` = self else { return }
+        let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+        let scene = GameScene(size: size)
+        self.view?.presentScene(scene, transition:reveal)
+      }
+      ]))
+   }
+  
+  // 6
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
---
+++ Hoborush/animazioni.swift
@@ -0,0 +1,42 @@
+//
+//  animazioni.swift
+//  Hoborush
+//
+//  Created by Ferdinando Carbone on 08/12/22.
+//
+
+import SpriteKit
+
+public func deathAn()
+{
+    var deadArr: [SKTexture] = []
+    for i in 1...13 {
+        deadArr.append(SKTexture(imageNamed: "HoboDying\(i)"))
+    }
+    deathAnimation = SKAction.animate(with: deadArr, timePerFrame: 0.03)
+}
+public func AlienWalkAn()
+{
+    var alienWArr: [SKTexture] = []
+    for i in 1...7 {
+        alienWArr.append(SKTexture(imageNamed: "AlienWalking\(i)"))
+    }
+    alienWalkingAnimation = SKAction.repeatForever(SKAction.animate(with: alienWArr, timePerFrame: 0.03))
+}
+public func idleAnimation()
+{
+    var idleArr: [SKTexture] = []
+    for i in 1...5 {
+        idleArr.append(SKTexture(imageNamed: "HoboIdle\(i)"))
+    }
+    idleanimation = SKAction.repeatForever(SKAction.animate(with: idleArr, timePerFrame: 0.15))
+}
+public func HoboAttack(){
+    var attArr: [SKTexture] = []
+    for i in 1...8 {
+        //var scaler = "HoboAttack\(i)"
+        //scaler.resize(
+        attArr.append(SKTexture(imageNamed: "HoboAttack\(i)"))
+    }
+    attAnimation = SKAction.animate(with: attArr, timePerFrame: 0.03)
+}
